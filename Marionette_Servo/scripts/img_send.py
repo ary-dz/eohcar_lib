437f6365aa97e7b46f81e587c4e1f617fccac2c3
Gautam Dayal <gautam.dayal@yahoo.com>
2025-04-03 21:28:43 -0500

    change

--- Marionette_Servo/scripts/img_send.py
+++ Marionette_Servo/scripts/img_send.py
@@ -1,10 +1,13 @@
+from picamera2 import Picamera2
 import cv2
 import socket
 import pickle
 import struct
 
-# Initialize camera
-cap = cv2.VideoCapture(0)
+# Initialize Pi Camera
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480)}))
+picam2.start()
 
 # Create socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,14 +16,14 @@ server_socket.listen(1)
 conn, addr = server_socket.accept()
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    frame = picam2.capture_array()
     
+    # Convert frame to BGR (Picamera2 outputs RGB by default)
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
     # Encode frame
     data = pickle.dumps(frame)
     conn.sendall(struct.pack("L", len(data)) + data)
 
-cap.release()
 conn.close()
 server_socket.close()
